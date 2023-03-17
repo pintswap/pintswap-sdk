b6168ed578d2473d4f4bc5eec30c4e14c5f5b117
pintswap <support@pintswap.eth>
2023-03-16 20:12:02 -0400

    update

--- lib/pintswap.d.ts
+++ lib/pintswap.d.ts
@@ -1,1 +1,11 @@
+import type { BytesLike } from "@ethersproject/bytes";
+import type { BigNumberish } from "@ethersproject/bignumber";
+interface IOffer {
+    givesToken: BytesLike;
+    getsToken: BytesLike;
+    givesAmount: BigNumberish;
+    getsAmount: BigNumberish;
+}
+export declare const createContractAsTaker: (offer: IOffer, maker: string, taker: string) => any;
+export declare const hashOffer: (o: any) => any;
 export {};
--- src.ts/pintswap.ts
+++ src.ts/pintswap.ts
@@ -2,8 +2,11 @@ import { protocol } from "./protocol";
 import { ZeroP2P } from "@zerodao/p2p";
 import { Signer } from "@ethersproject/abstract-signer";
 import { pipe } from "it-pipe";
+import * as lp from "it-length-prefixed";
 import type { BytesLike } from "@ethersproject/bytes";
 import type { BigNumberish } from "@ethersproject/bignumber";
+import { TPCEcdsaKeyGen } from "@safeheron/two-party-ecdsa-js";
+import { emasm } from "emasm";
 
 interface IOffer {
   givesToken: BytesLike;
@@ -11,6 +14,59 @@ interface IOffer {
   givesAmount: BigNumberish;
   getsAmount: BigNumberish;
 }
+
+export const createContractAsTaker = (offer: IOffer, maker: string, taker: string) => {
+  return emasm([
+    'pc',
+    'returndatasize',
+    '0x64',
+    'returndatasize',
+    'returndatasize',
+    offer.givesToken,
+    '0x23b872dd00000000000000000000000000000000000000000000000000000000',
+    'returndatasize',
+    'mstore',
+    maker,
+    '0x4',
+    'mstore',
+    taker,
+    '0x24',
+    'mstore',
+    offer.givesAmount,
+    '0x44',
+    'mstore',
+    'gas',
+    'call',
+    '0x0',
+    '0x0',
+    '0x64',
+    '0x0',
+    '0x0',
+    offer.getsToken,
+    taker,
+    '0x4',
+    'mstore',
+    maker,
+    '0x24',
+    'mstore',
+    offer.getsAmount,
+    '0x44',
+    'mstore',
+    'gas',
+    'call',
+    'and',
+    'failure',
+    'jumpi',
+     maker,
+    'selfdestruct',
+    ['failure', ['0x0', '0x0', 'revert']]
+  ])
+};
+  
+
+export const hashOffer = (o) => {
+  return solidityKeccak256(['address', 'address', 'uint256', 'uint256'], [getAddress(o.givesToken), getAddress(o.getsToken), o.givesAmount, o.getsAmount ]);
+};
 
 class Pintswap extends ZeroP2P {
   public signer: Signer;
@@ -25,13 +81,16 @@ class Pintswap extends ZeroP2P {
     signer
   }) {
     const self = new this({ signer });
-    await self.handle('/pintswap/0.1.0/orders', async (duplex) => {
-      await new Promise((resolve) => pipe(protocol.OfferList.encode({
-        offers: self.offers
-      }), duplex.stream.source, resolve)); 
-    });
-    await self.handle('/pintswap/0.1.0/create-trade', async () => {
-      // should do keygen and sign a tx between both parties
+    await self.handle('/pintswap/0.1.0/orders', (duplex) => pipe(duplex.sink, lp.encode(), protocol.OfferList.encode({ offers: self.offers })));
+    await self.handle('/pintswap/0.1.0/create-trade', async (duplex) => {
+      const context = await TPCEcdsaKeyGen.P2Context.createContext();
+      const message1 = await pipe(duplex.source, lp.decode());
+      const message2 = context.step1(message1);
+      await pipe(duplex.sink, lp.encode(), message2);
+      const message3 = await pipe(duplex.source, lp.decode());
+      context.step2(message3);
+      const key = JSON.stringify(context.exportKeyShare());
+      
     });
     await self.start();
     return self;

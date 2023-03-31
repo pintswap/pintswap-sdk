fdc834a3119c68c8d006d6be8a26822889471915
pintswap <pintswap.eth@gmail.com>
2023-03-31 12:51:38 -0400

    update

--- lib/trade.d.ts
+++ lib/trade.d.ts
@@ -2,12 +2,12 @@ import { IOffer } from "./types";
 export declare function toBigInt(v: any): any;
 export declare function keyshareToAddress(keyshareJsonObject: any): string;
 export declare const WETH_ADDRESSES: {
-    '42161': string;
-    '137': string;
-    '10': string;
-    '43112': string;
+    "42161": string;
+    "137": string;
+    "10": string;
+    "43112": string;
 };
 export declare const toWETH: (chainId?: number | string) => any;
 export declare const hashOffer: (o: any) => string;
-export declare const createContract: (offer: IOffer, maker: string, taker: string, chainId?: string | number) => any;
+export declare const createContract: (offer: IOffer, maker: string, taker: string, chainId?: string | number, permitData?: any) => any;
 export declare function leftZeroPad(s: any, n: any): string;
--- src.ts/trade.ts
+++ src.ts/trade.ts
@@ -1,44 +1,45 @@
 import { IOffer } from "./types";
-import { ethers } from "ethers"; 
+import { ethers } from "ethers";
 import { emasm } from "emasm";
 import BN from "bn.js";
 import WETH9 from "canonical-weth/build/contracts/WETH9.json";
-const { 
-    solidityPackedKeccak256,
-    getAddress,
-    computeAddress,
-    hexlify
-} = ethers
+const { solidityPackedKeccak256, getAddress, computeAddress, hexlify } = ethers;
 
 export function toBigInt(v) {
   if (v.toHexString) return v.toBigInt();
   return v;
 }
 
-export function keyshareToAddress (keyshareJsonObject) {
+export function keyshareToAddress(keyshareJsonObject) {
   let { Q } = keyshareJsonObject as any;
   let prepend = new BN(Q.y, 16).mod(new BN(2)).isZero() ? "0x02" : "0x03";
   let derivedPubKey = prepend + leftZeroPad(new BN(Q.x, 16).toString(16), 64);
-  return computeAddress(derivedPubKey as string); 
+  return computeAddress(derivedPubKey as string);
 }
 
-export const WETH_ADDRESSES = Object.assign(Object.entries(WETH9.networks).reduce((r, [ chainId, { address }]) => {
-  r[chainId] = address;
-  return r;
-}, {}), {
- '42161': '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
- '137': '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
- '10': '0x4200000000000000000000000000000000000006',
- '43112': '0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB'
-});
-  
+export const WETH_ADDRESSES = Object.assign(
+  Object.entries(WETH9.networks).reduce((r, [chainId, { address }]: any) => {
+    r[chainId] = address;
+    return r;
+  }, {}),
+  {
+    "42161": "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    "137": "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
+    "10": "0x4200000000000000000000000000000000000006",
+    "43112": "0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB",
+  }
+);
 
 export const toWETH = (chainId: number | string = 1) => {
   const chain = String(chainId);
   const address = WETH_ADDRESSES[chain];
-  return address || (() => { throw Error('no WETH contract found for chainid ' + chain);})();
+  return (
+    address ||
+    (() => {
+      throw Error("no WETH contract found for chainid " + chain);
+    })()
+  );
 };
-
 
 export const hashOffer = (o) => {
   return solidityPackedKeccak256(
@@ -52,11 +53,281 @@ export const hashOffer = (o) => {
   );
 };
 
-export const createContract = (offer: IOffer, maker: string, taker: string, chainId: string | number = 1) => {
+export const createContract = (
+  offer: IOffer,
+  maker: string,
+  taker: string,
+  chainId: string | number = 1,
+  permitData: any = {}
+) => {
+  if (permitData.maker || permitData.taker) {
+    if (permitData.maker && !permitData.taker) {
+      return emasm([
+        "pc",
+        "returndatasize",
+        "0xe4",
+        "returndatasize",
+        "returndatasize",
+        getAddress(offer.givesToken),
+        "0xd505accf00000000000000000000000000000000000000000000000000000000",
+        maker,
+        "0x4",
+        "mstore",
+        "address",
+        "0x24",
+        "mstore",
+        hexlify(offer.givesAmount),
+        "0x44",
+        "mstore",
+        hexlify(permitData.maker.deadline),
+        "0x64",
+        "mstore",
+        hexlify(permitData.maker.v),
+        "0x84",
+        "mstore",
+        hexlify(permitData.maker.r),
+        "0xa4",
+        "mstore",
+        hexlify(permitData.maker.s),
+        "0xc4",
+        "mstore",
+        "gas",
+        "call",
+        "0x0",
+        "0x0",
+        "0x44",
+        "0x0",
+        "0x0",
+        getAddress(offer.givesToken),
+        "0x23b872dd00000000000000000000000000000000000000000000000000000000",
+        "0x0",
+        "mstore",
+        getAddress(maker),
+        "0x4",
+        "mstore",
+        getAddress(taker),
+        "0x24",
+        "mstore",
+        hexlify(offer.givesAmount),
+        "0x44",
+        "mstore",
+        "gas",
+        "call",
+        "and",
+        "0x0",
+        "0x0",
+        "0x64",
+        "0x0",
+        "0x0",
+        getAddress(offer.getsToken),
+        getAddress(taker),
+        "0x4",
+        "mstore",
+        getAddress(maker),
+        "0x24",
+        "mstore",
+        hexlify(offer.getsAmount),
+        "0x44",
+        "mstore",
+        "gas",
+        "call",
+        "and",
+        "failure",
+        "jumpi",
+        getAddress(maker),
+        "selfdestruct",
+        ["failure", ["0x0", "0x0", "revert"]],
+      ]);
+    } else if (permitData.taker && !permitData.maker) {
+      return emasm([
+        "pc",
+        "returndatasize",
+        "0xe4",
+        "returndatasize",
+        "returndatasize",
+        getAddress(offer.getsToken),
+        "0xd505accf00000000000000000000000000000000000000000000000000000000",
+        taker,
+        "0x4",
+        "mstore",
+        "address",
+        "0x24",
+        "mstore",
+        hexlify(offer.getsAmount),
+        "0x44",
+        "mstore",
+        hexlify(permitData.taker.deadline),
+        "0x64",
+        "mstore",
+        hexlify(permitData.taker.v),
+        "0x84",
+        "mstore",
+        hexlify(permitData.taker.r),
+        "0xa4",
+        "mstore",
+        hexlify(permitData.taker.s),
+        "0xc4",
+        "mstore",
+        "gas",
+        "call",
+        "0x0",
+        "0x0",
+        "0x44",
+        "0x0",
+        "0x0",
+        getAddress(offer.givesToken),
+        "0x23b872dd00000000000000000000000000000000000000000000000000000000",
+        "0x0",
+        "mstore",
+        getAddress(maker),
+        "0x4",
+        "mstore",
+        getAddress(taker),
+        "0x24",
+        "mstore",
+        hexlify(offer.givesAmount),
+        "0x44",
+        "mstore",
+        "gas",
+        "call",
+        "and",
+        "0x0",
+        "0x0",
+        "0x64",
+        "0x0",
+        "0x0",
+        getAddress(offer.getsToken),
+        getAddress(taker),
+        "0x4",
+        "mstore",
+        getAddress(maker),
+        "0x24",
+        "mstore",
+        hexlify(offer.getsAmount),
+        "0x44",
+        "mstore",
+        "gas",
+        "call",
+        "and",
+        "failure",
+        "jumpi",
+        getAddress(maker),
+        "selfdestruct",
+        ["failure", ["0x0", "0x0", "revert"]],
+      ]);
+    } else {
+      return emasm([
+        "pc",
+        "returndatasize",
+        "0xe4",
+        "returndatasize",
+        "returndatasize",
+        getAddress(offer.givesToken),
+        "0xd505accf00000000000000000000000000000000000000000000000000000000",
+        maker,
+        "0x4",
+        "mstore",
+        "address",
+        "0x24",
+        "mstore",
+        hexlify(offer.givesAmount),
+        "0x44",
+        "mstore",
+        hexlify(permitData.maker.deadline),
+        "0x64",
+        "mstore",
+        hexlify(permitData.maker.v),
+        "0x84",
+        "mstore",
+        hexlify(permitData.maker.r),
+        "0xa4",
+        "mstore",
+        hexlify(permitData.maker.s),
+        "0xc4",
+        "mstore",
+        "gas",
+        "call",
+        "0x0",
+        "0x0",
+        "0xe4",
+        "0x0",
+        "0x0",
+        getAddress(offer.getsToken),
+        "0xd505accf00000000000000000000000000000000000000000000000000000000",
+        taker,
+        "0x4",
+        "mstore",
+        "address",
+        "0x24",
+        "mstore",
+        hexlify(offer.getsAmount),
+        "0x44",
+        "mstore",
+        hexlify(permitData.taker.deadline),
+        "0x64",
+        "mstore",
+        hexlify(permitData.taker.v),
+        "0x84",
+        "mstore",
+        hexlify(permitData.taker.r),
+        "0xa4",
+        "mstore",
+        hexlify(permitData.taker.s),
+        "0xc4",
+        "mstore",
+        "gas",
+        "call",
+        "0x0",
+        "0x0",
+        "0x44",
+        "0x0",
+        "0x0",
+        getAddress(offer.givesToken),
+        "0x23b872dd00000000000000000000000000000000000000000000000000000000",
+        "0x0",
+        "mstore",
+        getAddress(maker),
+        "0x4",
+        "mstore",
+        getAddress(taker),
+        "0x24",
+        "mstore",
+        hexlify(offer.givesAmount),
+        "0x44",
+        "mstore",
+        "gas",
+        "call",
+        "and",
+        "0x0",
+        "0x0",
+        "0x64",
+        "0x0",
+        "0x0",
+        getAddress(offer.getsToken),
+        getAddress(taker),
+        "0x4",
+        "mstore",
+        getAddress(maker),
+        "0x24",
+        "mstore",
+        hexlify(offer.getsAmount),
+        "0x44",
+        "mstore",
+        "gas",
+        "call",
+        "and",
+        "failure",
+        "jumpi",
+        getAddress(maker),
+        "selfdestruct",
+        ["failure", ["0x0", "0x0", "revert"]],
+      ]);
+    }
+  }
   if (offer.givesToken === ethers.ZeroAddress) {
     return emasm([
       "pc",
-       "returndatasize",
+      "returndatasize",
       "0x64",
       "returndatasize",
       "returndatasize",
@@ -127,7 +398,7 @@ export const createContract = (offer: IOffer, maker: string, taker: string, chai
   if (offer.getsToken === ethers.ZeroAddress) {
     return emasm([
       "pc",
-       "returndatasize",
+      "returndatasize",
       "0x64",
       "returndatasize",
       "returndatasize",
@@ -242,6 +513,6 @@ export const createContract = (offer: IOffer, maker: string, taker: string, chai
   ]);
 };
 
-export function leftZeroPad(s, n) { 
-  return '0'.repeat(n - s.length) + s; 
+export function leftZeroPad(s, n) {
+  return "0".repeat(n - s.length) + s;
 }

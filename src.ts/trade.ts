940a38e5dffcf77a0f44f61edfc5d1ac467cd260
yoyobojo <angellolazar@gmail.com>
2023-03-31 12:43:27 -0400

    added mainnet

--- src.ts/trade.ts
+++ src.ts/trade.ts
@@ -26,6 +26,7 @@ export const WETH_ADDRESSES = Object.assign(Object.entries(WETH9.networks).reduc
   r[chainId] = address;
   return r;
 }, {}), {
+  '1': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
  '42161': '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
  '137': '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
  '10': '0x4200000000000000000000000000000000000006',

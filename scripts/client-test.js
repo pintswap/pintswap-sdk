f45ceff2e34514c9918140513a1186922d4ea9fa
yoyobojo <angellolazar@gmail.com>
2023-04-04 20:17:27 -0400

    maker now can provide weth as givesToken

--- scripts/client-test.js
+++ scripts/client-test.js
@@ -3,14 +3,16 @@ const ethers = hre.ethers;
 const ethersModule = require('ethers');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
-const { Pintswap, hashOffer } = require('../lib');
-const helpers = require("@nomicfoundation/hardhat-network-helpers");
+const WETH9 = require("canonical-weth/build/contracts/WETH9.json");
+const { Pintswap, hashOffer, setFallbackWETH } = require('../lib');
 const argv = yargs(hideBin(process.argv))
   .boolean(['mockMaker', 'mockTaker'])
   .argv;
 
 async function main() {
+  const testingEth = process.env.ETH ? true : false;
   const WALLET = process.env.WALLET || "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199";
+
   console.log(
     `Funding Wallet (${WALLET}) with 6000 ETH \n`
   );
@@ -28,7 +30,6 @@ async function main() {
   const client = await ethers.getSigner(WALLET);
 
   const TestTokenContractFactory = await ethers.getContractFactory('TestToken');
-
   const testToken = await TestTokenContractFactory.connect(client)
     .deploy(ethers.utils.parseEther('6000.0'), "DAI", "DAI");
 
@@ -36,17 +37,27 @@ async function main() {
 
   // =====> test maker
   if (argv.mockMaker) {
-    // mint maker owned token
+    // Mint maker owned token
     let [ makerSigner ] = await ethers.getSigners();
-    const makerOwnedTestToken = await TestTokenContractFactory.connect(makerSigner)
-      .deploy(ethers.utils.parseEther('6000.0'), "Wrapped Bitcoin", "WBTC");
 
-    await makerOwnedTestToken.deployed();
+    let makerOwnedTestToken;
+    let weth;
+    if(testingEth) {
+      const WETH9Factory = new ethers.ContractFactory(WETH9.abi, WETH9.bytecode, makerSigner);
+      makerOwnedTestToken = await WETH9Factory.connect(makerSigner).deploy();
+      weth = await makerOwnedTestToken.deployed();
+      setFallbackWETH(weth.address);
+      await weth.deposit({ value: '100' + '0'.repeat(18) });
+    } else {
+      makerOwnedTestToken = await TestTokenContractFactory.connect(makerSigner)
+        .deploy(ethers.utils.parseEther('6000.0'), "Wrapped Bitcoin", "WBTC");
+      await makerOwnedTestToken.deployed();
+    }
 
     const maker = await Pintswap.initialize({ signer: await (new ethersModule.JsonRpcProvider('http://localhost:8545').getSigner(0)) });
 
     const offer = { 
-      givesToken: makerOwnedTestToken.address,
+      givesToken: testingEth ? '0x' + '0'.repeat(40) : makerOwnedTestToken.address,
       getsToken: testToken.address,
       givesAmount: ethers.utils.parseUnits("50.0").toHexString(),
       getsAmount: ethers.utils.parseUnits("50.0").toHexString()
@@ -54,7 +65,7 @@ async function main() {
 
     console.log(`
       Offer<IOffer> : {
-      \t givesToken: ${ makerOwnedTestToken.address },
+      \t givesToken: ${ testingEth ? '0x' + '0'.repeat(40) : makerOwnedTestToken.address },
       \t getsTokens: ${ testToken.address },
       \t givesAmount: ${ ethers.utils.parseUnits("50.0").toHexString() },
       \t getsAmounts: ${ ethers.utils.parseUnits("50.0").toHexString() },
@@ -66,6 +77,7 @@ async function main() {
       Mock Maker MultiAddress: ${ maker.peerId.toB58String() } \n
       URL: http://localhost:3000/#/${maker.peerId.toB58String()}/${hashOffer(offer)} \n
       ${!process.env.WALLET ? `Test Wallet Private Key: ${WALLET}` : ``} \n
+      ${process.env.ETH ? `WETH Address: ${weth.address}` : ''}
     `)
 
     maker.broadcastOffer(offer);

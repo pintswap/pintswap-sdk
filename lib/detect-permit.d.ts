0834a24569ed843ec12bebdb7d891030994187eb
pintswap <support@pintswap.eth>
2023-04-18 13:16:41 -0400

    update

---
+++ lib/detect-permit.d.ts
@@ -0,0 +1,3 @@
+export declare function toProvider(o: any): any;
+export declare const createPermitTestContract: (permitData?: any) => any;
+export declare function detectPermit(address: string, provider: any): Promise<boolean>;

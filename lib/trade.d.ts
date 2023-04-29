f4bba9ad1a3b4c02b085d512f263696a74b4ca33
pintswap <dev@pintswap.exchange>
2023-04-29 17:54:46 -0500

    update

--- lib/trade.d.ts
+++ lib/trade.d.ts
@@ -32,6 +32,7 @@ export declare const WETH_ADDRESSES: {
     "137": string;
     "10": string;
     "43112": string;
+    "324": string;
 };
 export declare const setFallbackWETH: (address: any) => void;
 export declare const coerceToWeth: (address: any, signer: any) => Promise<any>;

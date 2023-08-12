2d6634d92e9dbdfeae1cd51678c9f63ceefb9a17
pintswap <dev@pintswap.exchange>
2023-08-12 17:03:21 +0000

    update

---
+++ lib/chains.d.ts
@@ -0,0 +1,17 @@
+import { ethers } from "ethers";
+export declare const WETH_ADDRESSES: {
+    "42161": string;
+    "137": string;
+    "10": string;
+    "43112": string;
+    "324": string;
+    "42220": string;
+};
+export declare const NETWORKS: {
+    name: string;
+    chainId: number;
+    provider: ethers.JsonRpcProvider;
+}[];
+export declare function tokenExists(provider: any, address: any): Promise<boolean>;
+export declare function detectTradeNetwork(trade: any): Promise<number>;
+export declare function detectTokenNetwork(address: string): Promise<number>;
---
+++ src.ts/chains.ts
@@ -0,0 +1,72 @@
+import { ethers } from "ethers";
+import WETH9 from "canonical-weth/build/contracts/WETH9.json";
+
+export const WETH_ADDRESSES = Object.assign(
+  Object.entries(WETH9.networks).reduce((r, [chainId, { address }]: any) => {
+    r[chainId] = address;
+    return r;
+  }, {}),
+  {
+    "42161": "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    "137": "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
+    "10": "0x4200000000000000000000000000000000000006",
+    "43112": "0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB",
+    "324": "0x8Ebe4A94740515945ad826238Fc4D56c6B8b0e60",
+    "42220": "0x122013fd7dF1C6F636a5bb8f03108E876548b455"
+  }
+);
+
+export const NETWORKS = [{
+  name: "Ethereum",
+  chainId: 1,
+  provider: new ethers.InfuraProvider("mainnet")
+}, {
+  name: "Optimism",
+  chainId: 10,
+  provider: new ethers.InfuraProvider("optimism")
+}, {
+  name: "Polygon",
+  chainId: 137,
+  provider: new ethers.InfuraProvider("matic")
+}, {
+  name: "Arbitrum",
+  chainId: 42161,
+  provider: new ethers.InfuraProvider("arbitrum")
+}, {
+  name: "Celo",
+  chainId: 42220,
+  provider: new ethers.JsonRpcProvider("https://forno.celo.org")
+}];
+
+export async function tokenExists(provider, address) {
+  const contract = new ethers.Contract(address, ['function name() view returns (string)', 'function symbol() view returns (string)'], provider);
+  if (await provider.getCode(address) === '0x') return false;
+  try {
+    await contract.symbol();
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+export async function detectTradeNetwork(trade: any) {
+  const [ gets, gives ] = [ 'gets', 'gives' ].map((v) => trade[v].token);
+  for (const { provider, chainId } of NETWORKS) {
+    if (gets !== ethers.ZeroAddress) {
+      if (!await tokenExists(provider, gets)) continue;
+    } 
+    if (gives !== ethers.ZeroAddress) {
+      if (!await tokenExists(provider, gives)) continue;
+    } 
+    return chainId;
+  }
+  return 0;
+}
+
+export async function detectTokenNetwork(address: string) {
+  if (address === ethers.ZeroAddress) return 1;
+  for (const { provider, chainId } of NETWORKS) {
+    if (await tokenExists(provider, address)) return chainId;
+  }
+  return 0;
+}

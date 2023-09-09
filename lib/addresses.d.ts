3e96bfdcbde7edc1f09c1e90ed5af7dda1150f5e
pintswap <dev@pintswap.exchange>
2023-09-09 23:44:38 +0000

    update

---
+++ lib/addresses.d.ts
@@ -0,0 +1,4 @@
+export declare const DEPLOYMENTS: any;
+export declare const TEST_MNEMONIC = "assume fitness moment future coin dutch wait join delay faint response skin";
+export declare function computeAddresses(deployerAddress: string): any;
+export declare function computeAndSaveAddresses(deployerAddress: string): any;
---
+++ src.ts/addresses.ts
@@ -0,0 +1,29 @@
+
+import { ethers } from "ethers";
+
+export const DEPLOYMENTS: any = {};
+
+export const TEST_MNEMONIC = 'assume fitness moment future coin dutch wait join delay faint response skin';
+
+export function computeAddresses(deployerAddress: string) {
+  const result: any = {};
+  result.OPPS = ethers.getCreateAddress({
+    from: deployerAddress,
+    nonce: 0
+  });
+  const pintDeployer = ethers.getCreateAddress({
+    from: deployerAddress,
+    nonce: 1
+  });
+  result.PINT = ethers.getCreateAddress({
+    from: pintDeployer,
+    nonce: 3
+  });
+  return result;
+}
+
+export function computeAndSaveAddresses(deployerAddress: string) {
+  return Object.assign(DEPLOYMENTS, computeAddresses(deployerAddress));
+}
+
+computeAndSaveAddresses(ethers.Wallet.fromPhrase(TEST_MNEMONIC).address);

3a88bf7ab0de5a2309fa5455f816f3c9ed30d7a4
alo <0xyoyobojo@gmail.com>
2023-10-12 10:22:07 -0400

    updated script

--- scripts/get-addresses.js
+++ scripts/get-addresses.js
@@ -1,8 +1,7 @@
-const { computeAddresses } = require("../lib");
+const { computeAddresses, TEST_MNEMONIC } = require("../lib");
 const { ethers } = require("ethers");
 
 (async () => {
-  const TEST_MNEMONIC = 'assume fitness moment future coin dutch wait join delay faint response skin';
   const addresses = computeAddresses(ethers.Wallet.fromPhrase(TEST_MNEMONIC).address);
   console.log("Contract Addresses", addresses);
 })().catch(err => console.log(err))

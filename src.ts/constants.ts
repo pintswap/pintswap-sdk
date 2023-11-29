bb8a8e65f1a03803d3e39fc540517f7a389467b9
alo <0xyoyobojo@gmail.com>
2023-11-28 20:33:24 -0500

    revert signal server

--- src.ts/constants.ts
+++ src.ts/constants.ts
@@ -1,6 +1,6 @@
 export const SIGNAL_SERVER =
-  "/dns4/ns1.doublecup.dev/tcp/443/wss/p2p-webrtc-star/";
-// New: /dns4/p2p.diacetyl.is/tcp/443/wss/p2p-webrtc-star/'
+  "/dns4/p2p.diacetyl.is/tcp/443/wss/p2p-webrtc-star/";
+// New: "/dns4/ns1.doublecup.dev/tcp/443/wss/p2p-webrtc-star/"
 export const NAMESERVERS = {
   DRIP: "pint1zgsfhgpxt9kmeyxl2lm08l9nr2233gcahzhyllrfre7k4ce9vjywrpqxv2kgc",
   // New: PintP2P.toAddress("QmTABj5y3Q7LPErKeEPyNHKakp4gAknfwFEAm6LsD6TaNT")

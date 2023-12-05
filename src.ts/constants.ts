729a9875e7f6e94e7f8b4aa047fdcaeeac72c0d3
alo <0xyoyobojo@gmail.com>
2023-12-05 06:33:55 -0500

    revert ns

--- src.ts/constants.ts
+++ src.ts/constants.ts
@@ -1,9 +1,9 @@
-import { PintP2P } from "./p2p";
+// import { PintP2P } from "./p2p";
 
 export const SIGNAL_SERVER =
   "/dns4/p2p.diacetyl.is/tcp/443/wss/p2p-webrtc-star/";
 // "/dns4/ns1.doublecup.dev/tcp/443/wss/p2p-webrtc-star/";
 export const NAMESERVERS = {
-  // DRIP: "pint1zgsfhgpxt9kmeyxl2lm08l9nr2233gcahzhyllrfre7k4ce9vjywrpqxv2kgc",
-  DRIP: PintP2P.toAddress("QmTABj5y3Q7LPErKeEPyNHKakp4gAknfwFEAm6LsD6TaNT")
+  DRIP: "pint1zgsfhgpxt9kmeyxl2lm08l9nr2233gcahzhyllrfre7k4ce9vjywrpqxv2kgc",
+  // DRIP: PintP2P.toAddress("QmTABj5y3Q7LPErKeEPyNHKakp4gAknfwFEAm6LsD6TaNT")
 };

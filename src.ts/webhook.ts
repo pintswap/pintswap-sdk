f673d067e527c22cde2f6290cf0d1fbfffc20f26
Ian <ianhaley0@gmail.com>
2023-12-20 10:51:32 -0500

    taker percentage complete

--- src.ts/webhook.ts
+++ src.ts/webhook.ts
@@ -1,8 +1,119 @@
 import { Provider, ethers } from "ethers";
 import { networkFromChainId, providerFromChainId } from "./chains";
-import { displayOffer, getDecimals, getName, getSymbol } from "./utils";
+import {
+  displayOffer,
+  getDecimals,
+  getName,
+  getSymbol,
+  getUsdPrice,
+  percentChange,
+  getEthPrice
+} from "./utils";
 import { IOffer } from "./types";
 import { hashOffer } from "./trade";
+
+const mockHousePint1: IOffer = {
+  gets: {
+    amount: "0x2813e63b4531bc0000",
+    token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee"
+  },
+  gives: {
+    amount: "0x41608eb4fc6cf400",
+    token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0"
+  }
+
+}
+
+//0: Object { gives: {…}, gets: {…} }
+
+// gets: Object { amount: "0x2813e63b4531bc0000", token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee" }
+// ​​​
+// gives: Object { amount: "0x41608eb4fc6cf400", token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0" }
+// ​​​
+// <prototype>: Object { … }
+const mockHousePint2: IOffer = {
+  gets: {
+    amount: "0xa2d5e3e6bc69480000",
+    token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee"
+  },
+  gives: {
+    amount: "0x0105823ad3f1b3d000",
+    token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0"
+  }
+
+}
+// ​​
+// 1: Object { gives: {…}, gets: {…} }
+// ​​​
+// gets: Object { amount: "0xa2d5e3e6bc69480000", token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee" }
+// ​​​
+// gives: Object { amount: "0x0105823ad3f1b3d000", token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0" }
+// ​​​
+// <prototype>: Object { … }
+const mockHousePint3: IOffer = {
+  gets: {
+    amount: "0x01740f6978e11a500000",
+    token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee"
+  },
+  gives: {
+    amount: "0x024c65045cdfd48000",
+    token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0"
+  }
+
+}
+// ​​
+// 2: Object { gives: {…}, gets: {…} }
+// ​​​
+// gets: Object { amount: "0x01740f6978e11a500000", token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee" }
+// ​​​
+// gives: Object { amount: "0x024c65045cdfd48000", token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0" }
+// ​​​
+// <prototype>: Object { … }
+const mockHousePint4: IOffer = {
+  gets: {
+    amount: "0x029f89e7682eb8200000",
+    token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee"
+  },
+  gives: {
+    amount: "0x041608eb4fc6cf4000",
+    token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0"
+  }
+
+}
+// ​​
+// 3: Object { gives: {…}, gets: {…} }
+// ​​​
+// gets: Object { amount: "0x029f89e7682eb8200000", token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee" }
+// ​​​
+// gives: Object { amount: "0x041608eb4fc6cf4000", token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0" }
+// ​​​
+// <prototype>: Object { … }
+const mockHousePint5: IOffer = {
+  gets: {
+    amount: "0x04290ece2b20b6c00000",
+    token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee"
+  },
+  gives: {
+    amount: "0x06626defaca6a40000",
+    token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0"
+  }
+
+}
+// ​​
+// 4: Object { gives: {…}, gets: {…} }
+// ​​​
+// gets: Object { amount: "0x04290ece2b20b6c00000", token: "0x58fB30A61C218A3607e9273D52995a49fF2697Ee" }
+// ​​​
+// gives: Object { amount: "0x06626defaca6a40000", token: "0xFa4bAa6951B6Ee382e9ff9AF2D523278b99ca6D0" }
+// ​​​
+// <prototype>: Object { … }
+
+//https://discord.com/api/webhooks/1181339596647301171/rYNmPeF4GTMpB-pIFjqd9pH-Nt1-hi3iHykNF2FWI8wJqRxIVgRq84dONz2ly6sYKP9Q
+
+const TELEGRAM = {
+  code:"6551006929:AAGG7R8nPIMIwMK8o7-nKZ6oIwCm3wVnuJo"
+}
+
 
 const DISCORD = {
   base: "https://discord.com/api/webhooks",
@@ -16,6 +127,11 @@ const DISCORD = {
     token:
       "M0sqgaU0jOFyteIh6SKTLHTk9F2SMKPtWwPnRImrz0pZRQrhcInMP3fH80kO2ekncYbD",
   },
+  ian: {
+    id: "1181339596647301171",
+    token: 
+      "rYNmPeF4GTMpB-pIFjqd9pH-Nt1-hi3iHykNF2FWI8wJqRxIVgRq84dONz2ly6sYKP9Q"
+  }
 };
 
 const POST_REQ_OPTIONS = {
@@ -94,8 +210,16 @@ const getTokensFromTxHash = async (
   }
 };
 
-const calculateDiscount = (offer: IOffer) => {
-  console.log(offer)
+const calculateDiscount = async (offer: IOffer, chainId) => {
+  const { gets, gives } = await displayOffer(offer, chainId, "symbol");
+  const eth = await getEthPrice()
+  const givesUSD = await getUsdPrice(gives.token, eth)
+  const getsUSD = await getUsdPrice(gets.token, eth)
+  const originalPrice = Number(gets.amount) * getsUSD
+  const discountPrice = Number(gives.amount) * givesUSD
+  const discount = ((originalPrice - discountPrice) / originalPrice) * 100
+  const response = Number(discount.toFixed(2))
+  return response
 }
 
 const buildFulfillMarkdownLink = (
@@ -127,9 +251,8 @@ export const webhookRun = async function ({
     try {
       const provider = providerFromChainId(chainId);
       const tokens = await getTokensFromTxHash(txHash, provider, chainId);
-
       await fetch(
-        `${DISCORD.base}/${DISCORD.completed.id}/${DISCORD.completed.token}`,
+        `${DISCORD.base}/${DISCORD.ian.id}/${DISCORD.ian.token}`,
         {
           ...POST_REQ_OPTIONS,
           body: JSON.stringify({
@@ -160,9 +283,8 @@ export const webhookRun = async function ({
                   },
                   {
                     name: "",
-                    value: `[View in Explorer](${
-                      networkFromChainId(chainId).explorer
-                    }tx/${txHash})`,
+                    value: `[View in Explorer](${networkFromChainId(chainId).explorer
+                      }tx/${txHash})`,
                   },
                 ],
                 timestamp: new Date().toISOString(),
@@ -176,55 +298,59 @@ export const webhookRun = async function ({
     }
   }
   if (offer) {
-    const discount = calculateDiscount(offer)
+    const discount = await calculateDiscount(offer, chainId)
+    console.log("discount:", discount)
     try {
-      /**
-       * TODO
-       * check if there is at least a 3% discount on the taker end
-       * only send offers that are at a discount to the channels
-       */
-      const { gives, gets } = await displayOffer(offer, chainId, "name");
-
-      await fetch(`${DISCORD.base}/${DISCORD.new.id}/${DISCORD.new.token}`, {
-        ...POST_REQ_OPTIONS,
-        body: JSON.stringify({
-          content: "",
-          embeds: [
-            {
-              title: "👀 New Offer 👀",
-              color: 10181046,
-              fields: [
-                {
-                  name: `${gives.token}`,
-                  value: `${gives.amount}`,
-                  inline: true,
-                },
-                {
-                  name: "",
-                  value: "--->",
-                  inline: true,
-                },
-                {
-                  name: `${gets.token}`,
-                  value: `${gets.amount}`,
-                  inline: true,
-                },
-                {
-                  name: "Chain",
-                  value: `${networkFromChainId(chainId).name}`,
-                },
-                {
-                  name: "",
-                  value: buildFulfillMarkdownLink(offer, peer, chainId),
-                },
-              ],
-              timestamp: new Date().toISOString(),
-            },
-          ],
-        }),
-      });
+      if (discount >= 3) {
+        const { gives, gets } = await displayOffer(offer, chainId, "name");
+        
+        await fetch(`${DISCORD.base}/${DISCORD.ian.id}/${DISCORD.ian.token}`, {
+          ...POST_REQ_OPTIONS,
+          body: JSON.stringify({
+            content: "",
+            embeds: [
+              {
+                title: "👀 New Offer 👀",
+                color: 10181046,
+                fields: [
+                  {
+                    name: `${gives.token}`,
+                    value: `${gives.amount}`,
+                    inline: true,
+                  },
+                  {
+                    name: "",
+                    value: "--->",
+                    inline: true,
+                  },
+                  {
+                    name: `${gets.token}`,
+                    value: `${gets.amount}`,
+                    inline: true,
+                  },
+                  {
+                    name:"Discount",
+                    value:`%${discount}`,
+                  },
+                  {
+                    name: "Chain",
+                    value: `${networkFromChainId(chainId).name}`,
+                  },
+                  {
+                    name: "",
+                    value: buildFulfillMarkdownLink(offer, peer, chainId),
+                  },
+                ],
+                timestamp: new Date().toISOString(),
+              },
+            ],
+          }),
+        });
+      }
     } catch (e) {
       console.error(e);
     }
   }
 };
+
+webhookRun({ offer: mockHousePint4, chainId: 1 })
